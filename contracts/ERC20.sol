4aafbfccf1dba2056fa44e3ce897b064cf28d678
Zane Starr <zcstarr@gmail.com>
2020-02-26 14:59:04 -0800

    First Wave of Comments!

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -11,15 +11,20 @@ contract TutorialToken is ERC20 {
 	string public symbol = "TT";
 	uint8 public decimals = 2;
 	uint256 public INITAL_SUPPLY = 420;
-	ERC20 public token;
-	address payable public wallet;
+	
+	ERC20 public token; // REMOVE this line
+	
+	// rename this to owner payable shouldn't be required now 
+	address public owner;
+	
 	uint256 public rate;
 	uint256 public weiRaised;
 
       
 	constructor() public {
 		rate = 10;
-		wallet = msg.sender;
+		// this would be owner
+		owner = msg.sender;
 	}
 
 	function stabilizeRate() public {
@@ -30,14 +35,28 @@ contract TutorialToken is ERC20 {
 		_transfer(from, to, value);
 	}
 
-	function buyExampleToken(address _beneficiary) public payable {
-		require(_beneficiary != address(0));
+	// The refactor here would probably be to not include beneficiary unless you need to buy a token on someone's behalf
+	function buyExampleToken() public payable {
 		require(msg.value != 0);
-		uint256 tokensToETC = msg.value.mul(rate);
-		weiRaised = weiRaised.add(msg.value);
-		_mint(_beneficiary, tokensToETC);
-		token.transfer(_beneficiary, tokensToETC);
-		wallet.transfer(msg.value);
+
+		uint256 tokensToETC = msg.value.mul(rate); // Refactor to a more clear name ex. numTokens?
+		//refactored weiRaised to use balance instead
+		uint256 nextBalance = address(this).balance.add(msg.value);
+		// Here is a good place for a security/sanity check its ETC is MONEEY!
+		require(nextBalance > address(this).balance);
+		//refactor _beneficiary to msg.sender
+		
+		_mint(msg.sender, tokensToETC);
+		
+		// We are removing the transfer function from this
+		// token.transfer(msg.sender, tokensToETC);
+		
+		//wallet should be owner , this will take all the users money and give it to the owner :D !!
+		//owner.transfer(msg.value);
+		/*
+			we might implement a withdraw function where we call owner.transfer
+		 */
+		
 		stabilizeRate();
 	}
 }
